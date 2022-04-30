dbdb668f156d843e878daf0ca3de74a669bfe4b7
Kyle Canamar <canamar.kyle@gmail.com>
2022-04-30 16:20:27 -0700

    creates the data, creates wetherforcast component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./styles.css";
+import weatherData from "./weatherData";
+import WeatherForcast from "./WeatherForcast"
 
 export default function App() {
+  console.log(weatherData)
   return (
     <div className="App">
       <h1>My WeatherIcons App</h1> 
---
+++ src/weatherData.js
@@ -0,0 +1,24 @@
+const weatherData = [
+    {
+        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/'day'.svg",
+        conditions: "sunny",
+        time: "day"
+    },{
+        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg",
+        conditions: "clear",
+        time: "day"
+    },{
+        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg",
+        conditions: "clear",
+        time: "day"
+    },{
+        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-'day'_t7ckxp.svg",
+        conditions: "partly sunny",
+        time: "night"
+    },{
+        img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg',
+        conditions: "clear",
+        time: "day"
+    }
+]
+export default weatherData
